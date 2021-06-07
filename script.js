7c3ab22eb5a369acabf4b883276ad2b2146c16ce
sonia-ko <sonia-k@ukr.net>
2021-06-07 23:53:21 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// setInterval(function () {
+//   const now = new Date();
+//   const min = now.getMinutes();
+//   const hours = now.getHours();
+//   const sec = now.getSeconds();
+//   console.log(`${hours}:${min}:${sec}`);
+// }, 1000);
